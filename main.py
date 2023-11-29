fa028c11dce7e413ac9f4bf8cbf57ba5429668db
DevelopedBy-Siva <developedby.siva@gmail.com>
2023-11-29 13:00:03 +0530

    Integrate YOLO model with OpenCV to detect objects in real time

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-print("Hello")
+import torch
+import numpy as np
+import cv2
+
+# Loading the YOLOv5 model from the Ultralytics repository
+yolo_model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
+
+# Performs object detection and render the result
+image_results = yolo_model("resources/example.jpg")
+# image_results.show()
+
+# Detect objects in real time
+video_capture = cv2.VideoCapture(0)
+while video_capture.isOpened():
+    ret, video_frame = video_capture.read()
+
+    # Integrate with YOLO model
+    real_time_results = yolo_model(video_frame)
+    cv2.imshow('Detect', np.squeeze(real_time_results.render()))
+    if cv2.waitKey(10) & 0xFF == ord('q'):
+        break
+video_capture.release()
+cv2.destroyAllWindows()
