2865cd7892d304de6c769076f0b282ebf45c388b
DevelopedBy-Siva <developedby.siva@gmail.com>
2024-04-12 20:31:57 +0400

    Train new model & refactor the code

--- main.py
+++ main.py
@@ -33,22 +33,26 @@ def extract_eye_region_for_prediction(frame):
         if x_range > y_range:
             right_bound = round(0.5 * x_range) + x_max
             left_bound = x_min - round(0.5 * x_range)
-            bottom_bound = round((right_bound - left_bound - y_range) / 2) + y_max
+            bottom_bound = round(
+                (right_bound - left_bound - y_range) / 2) + y_max
             top_bound = y_min - round((right_bound - left_bound - y_range) / 2)
         else:
             bottom_bound = round(0.5 * y_range) + y_max
             top_bound = y_min - round(0.5 * y_range)
-            right_bound = round((bottom_bound - top_bound - x_range) / 2) + x_max
-            left_bound = x_min - round((bottom_bound - top_bound - x_range) / 2)
+            right_bound = round(
+                (bottom_bound - top_bound - x_range) / 2) + x_max
+            left_bound = x_min - \
+                round((bottom_bound - top_bound - x_range) / 2)
 
         # Draw rectangle around the eye
         cv2.rectangle(
-            frame, (left_bound, top_bound), (right_bound, bottom_bound), (255, 0, 0), 2
+            frame, (left_bound, top_bound), (right_bound,
+                                             bottom_bound), (255, 0, 0), 2
         )
 
     # Crop the image according to the determined coordinates
     cropped_eye_region = frame[
-        top_bound : bottom_bound + 1, left_bound : right_bound + 1
+        top_bound: bottom_bound + 1, left_bound: right_bound + 1
     ]
 
     # Resize the cropped image to 80x80 pixels
@@ -107,62 +111,17 @@ while True:
     # Display status based on the prediction ("Open Eyes" or "Closed Eyes")
     if prediction < 0.5:
         blink_counter = 0
-        status = "Open"
 
     else:
         blink_counter += 1
-        status = "Closed"
 
         # If the blink counter exceeds 2, show an alert for drowsiness
         if blink_counter > 2:
             # Show alert message on Screen
-            cv2.rectangle(
-                frame,
-                (round(width / 2) - 160, round(height) - 200),
-                (round(width / 2) + 175, round(height) - 120),
-                (0, 0, 255),
-                -1,
-            )
-            cv2.putText(
-                frame,
-                "Blink, Don't Sleep!",
-                (round(width / 2) - 136, round(height) - 146),
-                cv2.FONT_HERSHEY_SIMPLEX,
-                1,
-                (255, 255, 255),
-                2,
-                cv2.LINE_4,
-            )
-            cv2.imshow("Drowsiness Detection", frame)
+            print("DROWSY")
             k = cv2.waitKey(1)
             blink_counter = 1
             continue
-
-    # Show prediction on Screen
-    cv2.putText(
-        frame,
-        f"{prediction}",
-        (10, 30),
-        cv2.FONT_HERSHEY_SIMPLEX,
-        0.7,
-        (255, 0, 0),
-        2,
-        cv2.LINE_AA,
-    )
-    # Show status (Open or Closed on Screen)
-    cv2.putText(
-        frame,
-        "Status: " + status,
-        (10, 70),
-        cv2.FONT_HERSHEY_SIMPLEX,
-        1,
-        (255, 0, 0),
-        2,
-        cv2.LINE_AA,
-    )
-
-    # Display the processed frame
-    cv2.imshow("Drowsiness Detection", frame)
 
     # Exit the loop on pressing the 'Esc' key
     if cv2.waitKey(1) & 0xFF == 27:
